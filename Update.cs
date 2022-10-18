87a98d3b95b9adeb772dcc02b9c4914310981a48
kilinnik <kilinnikmixa99@gmail.com>
2022-10-18 08:14:20 +0300

    1

--- Update.cs
+++ Update.cs
@@ -18,70 +18,73 @@ namespace XpAndRepBot
             Debug.WriteLine(JsonSerializer.Serialize(update));
             using var db = new InfoContext();
             if (update.Message != null)
-            if (!update.Message.From.IsBot)
-            {
-                var idUser = update.Message.From.Id;
-                if (!db.TableUsers.Any(c => c.Id == idUser))
+                if (!update.Message.From.IsBot)
                 {
-                    db.Add(new Users(idUser, update.Message.From.FirstName + " " + update.Message.From.LastName, 0, 0));
-                }
-                db.SaveChanges();
-                var user = db.TableUsers.First(x => x.Id == idUser);
-                //if (update.Message.Caption != null) Console.WriteLine(update.Message.Caption.GetType());
-                //Console.WriteLine(update?.Message?.Text != null);
-                if (update?.Message?.Text != null || update?.Message?.Caption?.GetType() == typeof(string))
-                {
-                    if (update.Message.Caption == null) user.CurXp += update.Message.Text.ToString().Length;
-                    else user.CurXp += update.Message.Caption.Length;
-                    if (user.CurXp > Math.Genlvl(user.Lvl + 1))
+                    var idUser = update.Message.From.Id;
+                    if (!db.TableUsers.Any(c => c.Id == idUser))
                     {
-                        user.Lvl++;
-                        user.CurXp -= Math.Genlvl(user.Lvl);
-                        await botClient.SendTextMessageAsync(update.Message.Chat, $"{user.Name} получает {user.Lvl} lvl");
+                        db.Add(new Users(idUser, update.Message.From.FirstName + " " + update.Message.From.LastName, 0, 0));
                     }
                     db.SaveChanges();
-                }
+                    var user = db.TableUsers.First(x => x.Id == idUser);
+                    //if (update.Message.Caption != null) Console.WriteLine(update.Message.Caption.GetType());
+                    //Console.WriteLine(update?.Message?.Text != null);
+                    if (update?.Message?.Text != null || update?.Message?.Caption?.GetType() == typeof(string))
+                    {
+                        if (update.Message.Caption == null) user.CurXp += update.Message.Text.ToString().Length;
+                        else user.CurXp += update.Message.Caption.Length;
+                        if (user.CurXp > Math.Genlvl(user.Lvl + 1))
+                        {
+                            user.Lvl++;
+                            user.CurXp -= Math.Genlvl(user.Lvl);
+                            //await botClient.SendTextMessageAsync(update.Message.Chat, $"{user.Name} получает {user.Lvl} lvl");
+                            await botClient.SendTextMessageAsync(chatId: update.Message.Chat.Id, replyToMessageId: update.Message.MessageId, text: $"{user.Name} получает {user.Lvl} lvl");
+                        }
+                        db.SaveChanges();
+                    }
 
-                switch (update)
-                {
-                    case
+                    switch (update)
                     {
-                        Type: UpdateType.Message,
-                        Message: { Text: { } text, Chat: { } chat },
-                    } when text.Equals("/start", StringComparison.OrdinalIgnoreCase):
+                        case
                         {
-                            await botClient.SendTextMessageAsync(chat!, "Всем привет", cancellationToken: cancellationToken);
-                            break;
-                        }
-                    case
-                    {
-                        Type: UpdateType.Message,
-                        Message: { Text: { } text, Chat: { } chat },
-                    } when text.Equals("/me@XpAndRepBot"):
+                            Type: UpdateType.Message,
+                            Message: { Text: { } text, Chat: { } chat },
+                        } when text.Equals("/start", StringComparison.OrdinalIgnoreCase):
+                            {
+                                await botClient.SendTextMessageAsync(chat!, "Всем привет", cancellationToken: cancellationToken);
+                                break;
+                            }
+                        case
                         {
-                            await botClient.SendTextMessageAsync(chat!, Commands.Me(update, db), cancellationToken: cancellationToken);
-                            break;
-                        }
-                    case
-                    {
-                        Type: UpdateType.Message,
-                        Message: { Text: { } text, Chat: { } chat },
-                    } when text.Equals("/toplvl@XpAndRepBot"):
+                            Type: UpdateType.Message,
+                            Message: { Text: { } text, Chat: { } chat },
+                        } when text.Equals("/me@XpAndRepBot"):
+                            {
+                                //await botClient.SendTextMessageAsync(chat!, Commands.Me(update, db), cancellationToken: cancellationToken);
+                                await botClient.SendTextMessageAsync(chatId: update.Message.Chat.Id, replyToMessageId: update.Message.MessageId, text: Commands.Me(update, db));
+                                break;
+                            }
+                        case
                         {
-                            await botClient.SendTextMessageAsync(chat!, Commands.TopLvl(db), cancellationToken: cancellationToken);
-                            break;
-                        }
-                        //case
-                        //{
-                        //    Type: UpdateType.Message,
-                        //    Message.Chat: { } chat
-                        //}:
-                        //    {
-                        //        await botClient.SendTextMessageAsync(chat!, "", cancellationToken: cancellationToken);
-                        //        break;
-                        //    }
+                            Type: UpdateType.Message,
+                            Message: { Text: { } text, Chat: { } chat },
+                        } when text.Equals("/toplvl@XpAndRepBot"):
+                            {
+                                //await botClient.SendTextMessageAsync(chat!, Commands.TopLvl(db), cancellationToken: cancellationToken);
+                                await botClient.SendTextMessageAsync(chatId: update.Message.Chat.Id, replyToMessageId: update.Message.MessageId, text: Commands.TopLvl(db));
+                                break;
+                            }
+                            //case
+                            //{
+                            //    Type: UpdateType.Message,
+                            //    Message.Chat: { } chat
+                            //}:
+                            //    {
+                            //        await botClient.SendTextMessageAsync(chat!, "", cancellationToken: cancellationToken);
+                            //        break;
+                            //    }
+                    }
                 }
-            }
         }
         public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
